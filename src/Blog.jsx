60454ba0ea5e762aa5e6836b630486c5999e74c5
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-12-18 21:27:21 +0900

    チェックボックスの切替

---
+++ src/Blog.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Article from "./Article";
+class Blog extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isPublished: false,
+      order: 1
+    }
+  }
+
+  //公開状態を反転させる
+  togglePublished = () => {
+    this.setState({
+      isPublished: !this.state.isPublished
+    })
+  };
+  render(){
+    return(
+      <>
+        <Article title={"Reactの使い方"} isPublished={this.state.isPublished} toggle={() => this.togglePublished()}/>
+      </>
+    )
+  }
+}
+
+export default Blog
